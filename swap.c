e62f572f0dacf518e67c5e5788f1a704e6a598a4
antonio490 <asc490@hotmail.com>
2019-11-10 18:54:29 +0100

    swap.c

---
+++ swap.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// In C each function in a given scope have a unique name. 
+// There is not overloading
+
+void swap(int* i, int* j){
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void swap_double(double* i, double* j){
+    double temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int main(){
+
+    int m = 5, n = 10;
+    double x = 5.3, y = 10.6;
+    printf("inputs: %d, %d\n", m, n);
+    swap(&m, &n);
+    printf("outputs. %d, %d\n", m, n);
+    printf("double inputs: %lf, %lf\n", x, y);
+    swap_double(&x, &y);
+    printf("double outputs: %lf, %lf\n", x, y);
+}
