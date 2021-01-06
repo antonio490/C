3e6335e52e6855d4e5396380212c266e757ea9ee
antonio490 <asc490@hotmail.com>
2021-01-06 22:07:56 +0100

    subdirectories

---
+++ CMAKE/calculadora/build/CMakeFiles/calculadora.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/antonio/Escritorio/repositorios/C/CMAKE/calculadora/main.cpp" "/home/antonio/Escritorio/repositorios/C/CMAKE/calculadora/build/CMakeFiles/calculadora.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/antonio/Escritorio/repositorios/C/CMAKE/calculadora/build/my_math_dir/CMakeFiles/my_math.dir/DependInfo.cmake"
+  "/home/antonio/Escritorio/repositorios/C/CMAKE/calculadora/build/my_print_dir/CMakeFiles/my_print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
