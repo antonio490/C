9925391a59bda42a14dd97a018751f54174399b2
antonio490 <asc490@hotmail.com>
2021-02-28 14:10:41 +0100

    files and text exercises

--- csharp/filesExercises.cs
+++ csharp/filesExercises.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.IO;
+using
 
 namespace CSharpFundamentals{
 
@@ -24,6 +25,31 @@ namespace CSharpFundamentals{
 
         Console.WriteLine("File does not exist");
       }
+
+      public void exercise2(){
+
+        const string path = @"c:\temp\textfile.txt";
+
+        if(File.Exists(path)){
+
+          var text = File.ReadAllText(path);
+          var words = text.split(' ');
+          int wordSize = 0;
+          string longestWord = "";
+
+          foreach(string word in words){
+            if(word.Length > wordSize){}
+              longestWord = word;
+              wordSize = word.Length;
+            }
+          }
+
+          Console.WriteLine("Longest word is: " + longestWord);
+
+        }else{
+
+          Console.WriteLine("File does not exist");
+        }
 
 
 
