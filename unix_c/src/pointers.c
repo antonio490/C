73b74f75d501281c797f7b5f0c8c2ed4f0a324a9
antonio490 <asc490@hotmail.com>
2022-05-23 22:10:25 +0200

    update pointer example

--- unix_c/src/pointers.c
+++ unix_c/src/pointers.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <math.h>
 
 typedef struct T_STRUCT_POINT{
 
     float x;
     float y;
+    char *p_name;
 
 }t_s_point;
 
-typedef struct T_STRUCT_SQUARE{
+typedef struct T_STRUCT_RECT{
 
     t_s_point *p1;
     t_s_point *p2;
     t_s_point *p3;
     t_s_point *p4;
 
-}t_s_square;
+}t_s_rect;
 
 t_s_point* l_side( t_s_point *p1, t_s_point *p2 )
 {
@@ -32,22 +34,29 @@ t_s_point* l_side( t_s_point *p1, t_s_point *p2 )
 
 int main(int argc, char* argv[]){
 
-    t_s_square *square;
+    t_s_rect *square;
     t_s_point *p3;
 
+    square = (t_s_rect*) malloc(sizeof(t_s_rect));
     square->p1 = (t_s_point*) malloc(sizeof(t_s_point)); 
     square->p2 = (t_s_point*) malloc(sizeof(t_s_point)); 
 
+    square->p1->p_name = (char*)malloc(32);
+    square->p2->p_name = (char*)malloc(32);
+
+
     square->p1->x = 5.2;
     square->p1->y = 5.5;
+    strcpy( square->p1->p_name, "point one");
 
     square->p2->x = 10.4;
     square->p2->y = 5.5;
+    strcpy( square->p2->p_name, "point two");
 
     p3 = l_side( square->p1, square->p2); 
 
-    printf("Distance in x = %f \n", p3->x);
-    printf("Distance in y = %f \n", p3->y);
+    printf("Distance in x between %s and %s = %d \n", square->p1->p_name, square->p2->p_name, p3->x);
+    printf("Distance in y = %d \n", p3->y);
 
     return 0;
 }
