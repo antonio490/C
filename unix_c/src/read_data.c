6b4e2c81c8d4a60113c2a5bdcf3b73cedf829bf0
antonio490 <asc490@hotmail.com>
2022-05-30 21:32:08 +0200

    fix printf format

--- unix_c/src/read_data.c
+++ unix_c/src/read_data.c
@@ -4,21 +4,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main( int argc, char*argv[] )
+int main( int argc, char *argv[] )
 {
     int fd;
-    char filename[] = "read.data";
-    char data[45];
+    char filename[] = "/tmp/read.data";
+    char data[150];
     ssize_t br, bw;
 
     fd = open( filename, O_RDONLY );
     if( fd < 0 )
     {
-        printf("Can´t open data %s, error %s\n", filename, strerror( errno ));
+        printf("Cant open data %s, error %s\n", filename, strerror( errno ));
         exit( EXIT_FAILURE );
     }
 
-    br = read( fd, data, sizeof( data ) );
+    br = read(fd, data, sizeof( data ));
+
     if( br != sizeof( data ) )
     {
         if( br < 0 )
@@ -26,15 +27,18 @@ int main( int argc, char*argv[] )
             printf("Error reading file %s\n", strerror( errno ) );
             exit( EXIT_FAILURE );
         }else
-            printf("Could only read %d Bytes: %s\n", br);
+            printf("Could only read %d Bytes \n", br);
     }
 
+
     bw = write( STDOUT_FILENO, data, br );
+    
     if( bw != br )
     {
         printf("Could only write %d Bytes.\n", bw );
         exit( EXIT_FAILURE );
     }
+
 
     if( close( fd ) != 0 )
     {
