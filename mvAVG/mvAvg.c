eff6eb5a1703565d7697baabed30eefa3ce0c3f4
antonio490 <asc490@hotmail.com>
2023-02-05 17:39:56 +0100

    Function rollingAvg

--- mvAVG/mvAvg.c
+++ mvAVG/mvAvg.c
@@ -4,30 +4,38 @@
 #define WINDOW_SIZE 10
 
 
+float rollingAvg(int index, int len, float *temp_array)
+{
+    float sum = 0;
+
+    if(index >= WINDOW_SIZE) index = 0;
+    if(len >= WINDOW_SIZE) len = WINDOW_SIZE;
+
+    for(int i=0; i<WINDOW_SIZE; i++){
+        sum += temp_array[i];
+    }
+
+    return sum / len;
+}
+
+
 void main()
 {
     float temp_array[WINDOW_SIZE] = {0};
     int index = 0, len = 0;
-    float num, sum;
+    float num, avg;
 
     while(1)
     {
         printf("Enter temperature value: ");
         scanf("%f", &num);
+        
         temp_array[index] = num;
         index++;
         len++;
-        sum = 0;
 
-        if(index >= WINDOW_SIZE) index = 0;
-        if(len >= WINDOW_SIZE) len = WINDOW_SIZE;
-
-        for(int i=0; i<WINDOW_SIZE; i++){
-            sum += temp_array[i];
-        }
-
-        printf("\tSum: %.2f\t Length: %d\n", sum, len);
-        printf("\tAverage: %.2f\n", sum / len);
+        avg = rollingAvg(index, len, temp_array);
+        printf("\tAverage: %.2f\n", avg);
     }
 
 }
