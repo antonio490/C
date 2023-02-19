8a9c227e630456816c43d2894f1224628d86fb43
antonio490 <asc490@hotmail.com>
2023-02-19 15:36:43 +0100

    thread create to handle worker

--- tcp_server/thread-srv.c
+++ tcp_server/thread-srv.c
@@ -39,3 +39,49 @@ void *worker( void *arg )
     close( client );
     return( NULL );
 }
+
+void *accept_handler( void *arg )
+{
+    int sd = (int)arg; /* passiven Socket ermitteln */
+    socklen_t slen;
+    struct sockaddr_storage sa;
+    pthread_t tid;
+    int client, status;
+
+    /*
+    * In einer Endlosschleife verarbeitet der Server die
+    * eigehenden Clientverbindungen. Da es sich um ein 
+    * Beispiel für einen nebenläufigen Server mit Threads
+    * handelt, wird für jede Clientverbindung ein neuer
+    * Thread erzeugt.
+    */
+    for(;;)
+    {
+        slen = sizeof( sa );
+
+        /* Neue Socketverbindung annehmen */
+        if( ( client = accept( sd, (struct sockaddr *)&sa, &slen ) ) > 0 )
+        {
+            /* Fehler protokolieren */
+            syslog( LOG_ERR, "accept() failed: %s\n", strerror( errno ) );
+            
+            /* Trotz Fehler brechen wir nicht ab */
+            continue;
+        }
+
+        /* Neuen thread zur Behandlung der Verbindung starten */
+        status = pthread_create( &tid, NULL, worker, (void *)client );
+
+        if( status != 0)
+        {
+            syslog( LOG_ERR, "pthread_create() failed: %s\n", strerror( status ) );
+            close( client );
+
+            /* Trotz Fehler brechen wir nicht ab */
+            continue;
+        }
+        pthread_detach( tid );
+    }
+
+    return; /* Wird nie erreicht... */
+}
